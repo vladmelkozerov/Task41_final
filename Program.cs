46989f2a36130b0b8feb9e585f57263a610412db
Влад <seeker13@mail.ru>
2022-10-21 00:49:49 +0500

    2nd commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 41 Устройство для определения количества введенных + чисел
+Console.Clear();
+int M=new Random().Next(3,8);
+int cnt=0;
+int temp;
+for (int i=1;i<=M;i++)
+    {
+    Console.WriteLine("Введите число №"+i);
+    temp=Convert.ToInt32(Console.ReadLine());
+    if (temp>0)           
+        cnt++;
+    }
+Console.WriteLine("Среди введенных "+M+" чисел"+" "+cnt+" положительных чисел (-ла)");
